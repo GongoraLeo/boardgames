c274a678f925c4d0d2831adb75f4976342bb4044
GongoraLeo <gongora.leo@gmail.com>
2024-06-15 12:57:37 +0200

    tabla autor y editorial y FKs

--- bbdd.sql
+++ bbdd.sql
@@ -6,6 +6,20 @@ CREATE TABLE boardgames (
     id INT AUTO_INCREMENT PRIMARY KEY,
     estado VARCHAR(20) NOT NULL CHECK (type IN ('Own', 'Pre-ordered', 'For sale', 'Prepared')),
     nombre CHAR(50),
-    Autor
+    FOREIGN KEY (authors_id) REFERENCES authors(id),
+    FOREIGN KEY (editorial_id) REFERENCES editorial(id)
+);
+
+CREATE TABLE authors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre CHAR(50),
+    nacionalidad CHAR(50)
+
+);
+
+CREATE TABLE editorial (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre CHAR(50),
+    nacionalidad CHAR(50)
 
 );
